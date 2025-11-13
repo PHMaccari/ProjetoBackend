78247e408e1d43101e901f1670d1c42f6010d400
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-11-13 20:17:40 -0300

    Delete ProjetoBackend directory

--- ProjetoBackend/casa_automatica/src/main/java/viniccius13/casa_automatica/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package viniccius13.casa_automatica.repository;
-
-import viniccius13.casa_automatica.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
